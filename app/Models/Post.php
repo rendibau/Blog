f90bceb8351317dbedc65728c85f7d2aac03176b
xchweetz <nathanaeldwi2512@gmail.com>
2025-03-04 15:49:00 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
 
-class Post
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Nathanael Dwi Cahyo',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Nathanael Dwi Cahyo',
-                'body' => 'aaaaaaaLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-            ]
-            ];
-    }
-
-    public static function find($slug): array
-    {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable= ['title', 'author', 'slug', 'body'];
 }
