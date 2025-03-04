1adc58668c92cf9bb7d827d50f7ec034d951dbb2
xchweetz <nathanaeldwi2512@gmail.com>
2025-03-04 13:02:18 +0700

    Framework & Route Basic Logic

---
+++ routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Nathanael Dwi', 'title' => 'About']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Nathanael Dwi Cahyo',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Nathanael Dwi Cahyo',
+            'body' => 'aaaaaaaLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Nathanael Dwi Cahyo',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Nathanael Dwi Cahyo',
+            'body' => 'aaaaaaaLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]
+        ];
+
+        $post = Arr::first($posts, function($post) use ($slug) {
+            return $post['slug'] == $slug;
+        });
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
+
